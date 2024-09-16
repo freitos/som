baf7d31b4300a1e0c085ab3a3082fa319ee589da
freitos <danifreitas042006@gmail.com>
2024-09-16 16:43:21 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
+
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("body");
 
-btnSignin.addEventListener("click", function(){
-    body.className= "sign-in-js";
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
 });
 
-btnSignup.addEventListener("click", function(){
+btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
-});
+})
