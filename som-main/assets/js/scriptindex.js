30fb58701ebf3040257be791d1a313f4a1235fb4
freitos <danifreitas042006@gmail.com>
2024-12-04 14:00:04 -0300

    Delete som-main directory

--- som-main/assets/js/scriptindex.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuHamburger = document.getElementById("menu-hamburger");
-    const menuItems = document.querySelector(".menu-items");
-
-    menuHamburger.addEventListener("click", function () {
-        menuItems.classList.toggle("active"); // Mostra/oculta o menu
-    });
-});
